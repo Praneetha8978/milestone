853ae6ed171f5f813227ce6b08d23f7a6f061ce4
praneetha8978 <20b01a12b3@svecw.edu.in>
2022-12-17 23:41:06 +0530

    commitLevel4

---
+++ __tests__/todo.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+const todoList = require('../todo')
+
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList()
+describe('TodoList Test Suite', () => {
+  const formattedDate = d => {
+    return d.toISOString().split('T')[0]
+  }
+  const dateToday = new Date()
+  const today = formattedDate(dateToday)
+  const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+  )
+  const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+  )
+  beforeAll(() => {
+    add({ title: 'Test Today', completed: false, dueDate: new Date().toLocaleDateString('en-CA') })
+    add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
+    add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
+    add({ title: 'File taxes', dueDate: tomorrow, completed: false })
+    add({ title: 'Service Vehicle', dueDate: today, completed: false })
+  })
+  test('Should add new todo', () => {
+    const todoItemsCount = all.length
+    add({
+      title: 'Test Today',
+      completed: false,
+      dueDate: new Date().toLocaleDateString('en-CA')
+    })
+    expect(all.length).toBe(todoItemsCount + 1)
+  })
+  test('Mark As Complete', () => {
+    expect(all[0].completed).toBe(false)
+    markAsComplete(0)
+    expect(all[0].completed).toBe(true)
+  })
+  test('Checks Of Overdue Items', () => {
+    const over = overdue()
+    expect(all[1].title).toBe(over[0].title)
+    expect(all[1].dueDate).toBe(over[0].dueDate)
+  })
+  test('Checks Of DueToday Items', () => {
+    const due = dueToday()
+    expect(all[4].title).toBe(due[1].title)
+    expect(all[4].dueDate).toBe(due[1].dueDate)
+  })
+  test('Checks Of DueLater Items', () => {
+    const dueLater1 = dueLater()
+    expect(all[2].title).toBe(dueLater1[0].title)
+    expect(all[2].dueDate).toBe(dueLater1[0].dueDate)
+    expect(all[3].title).toBe(dueLater1[1].title)
+    expect(all[3].dueDate).toBe(dueLater1[1].dueDate)
+  })
+})
--- todo.js
+++ todo.js
@@ -1,4 +1,15 @@
 const todoList = () => {
+  const formattedDate = d => {
+    return d.toISOString().split('T')[0]
+  }
+  const dateToday = new Date()
+  const today = formattedDate(dateToday)
+  const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+  )
+  const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+  )
   const all = []
   const add = (todoItem) => {
     all.push(todoItem)
@@ -58,44 +69,4 @@ const todoList = () => {
 
   return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList }
 }
-
-const todos = todoList()
-
-const formattedDate = d => {
-  return d.toISOString().split('T')[0]
-}
-
-const dateToday = new Date()
-const today = formattedDate(dateToday)
-const yesterday = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() - 1))
-)
-const tomorrow = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() + 1))
-)
-
-todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
-todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
-
-console.log('My Todo-list\n\n')
-
-console.log('Overdue')
-const overdues = todos.overdue()
-const formattedOverdues = todos.toDisplayableList(overdues)
-console.log(formattedOverdues)
-console.log('\n\n')
-
-console.log('Due Today')
-const itemsDueToday = todos.dueToday()
-const formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-console.log(formattedItemsDueToday)
-console.log('\n\n')
-
-console.log('Due Later')
-const itemsDueLater = todos.dueLater()
-const formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-console.log(formattedItemsDueLater)
-console.log('\n\n')
+module.exports = todoList
